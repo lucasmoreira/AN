2c937ec8ea9a40d86adccf54fb4a8e7a3853c543
lucasmoreira <lucasmg_31@outlook.com>
2021-07-28 13:22:30 -0300

    prepare for deploy

--- app.py
+++ app.py
@@ -21,16 +21,16 @@ dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
 
 
 with st.beta_expander('Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy'):
-    texto = st.text_input("Insira texto corrigido")
+    texto = st.text_input("insira texto corrigido","Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy")
 with st.beta_expander('eirmod tempor invidunt ut labore et dolore magna aliquyam erat, '):
-    texto1 = st.text_input("Insira texto corrigido", key = 2)
+    texto1 = st.text_input("insira texto corrigido","eirmod tempor invidunt ut labore et dolore magna aliquyam erat, ")
 with st.beta_expander('sed diam voluptua. At vero eos et accusam et justo duo '):
-    texto2 = st.text_input("Insira texto corrigido",key = 3)
-with st.beta_expander('dolores et ea rebum. Stet clita kasd gubergren, no sea takimata'):
-    texto3 = st.text_input("Insira texto corrigido",key = 4)
+    texto2 = st.text_input("insira texto corrigido","sed diam voluptua. At vero eos et accusam et justo duo ")
+with st.beta_expander('dolor et ea rebum. Stet clita kasd gubergren, no sea takimata'):
+    texto3 = st.text_input("insira texto corrigido","dolores et ea rebum. Stet clita kasd gubergren, no sea takimata")
 with st.beta_expander('sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, '):
-    texto4 = st.text_input("Insira texto corrigido",key = 5)
+    texto4 = st.text_input("insira texto corrigido","sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet")
 with st.beta_expander('consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt '):
-    texto5 = st.text_input("Insira texto corrigido",key = 6)
+    texto5 = st.text_input("insira texto corrigido","consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ")
 
 st.button('Enviar')
